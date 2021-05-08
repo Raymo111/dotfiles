9b24f2973de0876cc0fc79c3e26a6e8f43638a79
Raymond Li <hi@raymond.li>
2021-05-07 23:30:26 -0400

    Fix typo
    Thanks @maxerenberg

--- scripts/img.sh
+++ scripts/img.sh
@@ -4,7 +4,7 @@ shopt -s expand_aliases
 
 # paste command
 alias paste='/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -sta "Add-Type -AssemblyName System.Windows.Forms;if ([Windows.Forms.Clipboard]::ContainsImage()) {[Windows.Forms.Clipboard]::GetImage().Save(\"${winpath}\", [System.Drawing.Imaging.ImageFormat]::Png) }"' # winpath is the var getting copied
-#alias paste='xclip -selection clipboard -o $path' # path is the var getting copied
+#alias paste='xclip -selection clipboard -o > $path' # path is the var getting copied
 
 # copy command
 function copy { echo -ne "$1" | clip.exe; }
