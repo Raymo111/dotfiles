c6a3ac6b278b2d2eb90cbbd466a68dce75cd7707
Raymond Li <hi@raymond.li>
2022-02-03 00:52:33 -0500

    Update autostart.sh

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -13,3 +13,4 @@
 #echo -e "document.getElementsByClassName('listItem-2P_4kh')[document.getElementsByClassName('listItem-2P_4kh').length-1].style.display='none',setInterval(\"Array.prototype.forEach.call(document.body.getElementsByTagName('*'),e=>{'rgb(47, 49, 54)'==window.getComputedStyle(e,null).getPropertyValue('color')&&(e.style.color='#000')})\",1e3)" | xclip -sel c
 #picom --vsync --backend glx
 xdotool key Num_Lock
+ksuperkey
