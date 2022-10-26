bf6427bc07023444ae38b78ac400ddd9b30c9311
Raymond Li <hi@raymond.li>
2022-10-25 23:46:05 -0400

    Run shellcheck on xautolock script

--- scripts/xautolock.sh
+++ scripts/xautolock.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# shellcheck disable=SC2154
 LOCK="$scriptdir/i3lock.sh"
-xidlehook --not-when-fullscreen --not-when-audio --timer 360 $LOCK '' --detect-sleep
+xidlehook --not-when-fullscreen --not-when-audio --timer 360 "$LOCK" '' --detect-sleep
