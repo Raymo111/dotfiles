0c2bc3958947c5a6672aa55136df03efdea93edb
Raymond Li <hi@raymond.li>
2022-10-25 23:45:07 -0400

    Run shellcheck on dislocker

--- scripts/dislocker.sh
+++ scripts/dislocker.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 part=""
 i=0
-drives=( $(sudo blkid | awk '{print substr($1, 0, length($1) - 1)}') )
+drives=()
+while IFS='' read -r line; do array+=("$line"); done < <(sudo blkid | awk '{print substr($1, 0, length($1) - 1)}')
 
 for drive in "${drives[@]}"; do
-	sudo dd if=$drive bs=128 count=1 status=none | hexdump -C | grep -q FVE-FS && bl[$i]=${drive} && ((i++))
+	sudo dd if="$drive" bs=128 count=1 status=none | hexdump -C | grep -q FVE-FS && bl[$i]=${drive} && ((i++))
 done
 
 if [[ ${#bl[@]} == 0 ]]; then
 	echo "Bitlocker drive not found."
 	exit
 elif [[ ${#bl[@]} == 1 ]]; then
-	read -n 1 -p "Unlock ${bl[0]}? [Y/n] " conf
+	read -rn 1 -p "Unlock ${bl[0]}? [Y/n] " conf
 	if [ "$conf" != "" ]; then echo; fi
 	if [ "$conf" = "${conf#[Nn]}" ]; then
 		part=${bl[0]}
@@ -23,15 +24,15 @@ else
 		echo "$((i+1)). $d"
 		((i++))
 	done
-	read -p "Drive to unlock [1-${#bl[@]}] " ans
+	read -rp "Drive to unlock [1-${#bl[@]}] " ans
 	if [ "$ans" != "" ]; then echo; fi
 	part=${bl[$((ans-1))]}
 fi
 if [[ $part != "" ]]; then
-	read -sp "User password for $part: " pass
+	read -rsp "User password for $part: " pass
 	[ -d /media/bitlocker ] || sudo mkdir /media/bitlocker
 	[ -d /media/bitlockermount ] || sudo mkdir /media/bitlockermount
-	sudo dislocker $part -u$pass -- /media/bitlocker
+	sudo dislocker "$part" -u"$pass" -- /media/bitlocker
 	sudo mount -o loop /media/bitlocker/dislocker-file /media/bitlockermount
 	echo "$part unlocked."
 	xdg-open /media/bitlockermount
