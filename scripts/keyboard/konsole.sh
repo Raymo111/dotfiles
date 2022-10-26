9ba9e728dfb801248ae40b0a25948ab80ea16363
Raymond Li <hi@raymond.li>
2022-10-25 23:45:43 -0400

    Run shellcheck on konsole script

--- scripts/keyboard/konsole.sh
+++ scripts/keyboard/konsole.sh
@@ -2,15 +2,16 @@
 FLAG=0
 while read -r line
 do
-	WINDOW_ID="$(echo  $line | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')"
-	NAME=$(xprop -id $WINDOW_ID | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
+	WINDOW_ID="$(echo  "$line" | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')"
+	NAME=$(xprop -id "$WINDOW_ID" | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
 	if [[ $NAME == *"Konsole" ]]
 	then
 		FLAG=1
-		$scriptdir/keyboard/konsole.py
+		# shellcheck disable=SC2154
+		"$scriptdir"/keyboard/konsole.py
 	elif [[ $FLAG == 1 ]]
 	then
 		FLAG=0
-		$scriptdir/keyboard/arch.py
+		"$scriptdir"/keyboard/arch.py
 	fi
 done < <(xprop -spy -root _NET_ACTIVE_WINDOW)
