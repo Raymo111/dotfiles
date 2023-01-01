9739c547e035cea1c5d679e4469c20fefae56d72
Raymond Li <hi@raymond.li>
2022-12-31 19:26:19 -0500

    Update keyboard script

--- scripts/keyboard/konsole.sh
+++ scripts/keyboard/konsole.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 FLAG=0
-while read -r line
-do
+while read -r line; do
 	WINDOW_ID="$(echo  "$line" | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')"
 	NAME=$(xprop -id "$WINDOW_ID" | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
-	if [[ $NAME == *"Konsole" ]]
-	then
+	if [[ $NAME == *"Konsole" && $FLAG == 0 ]]; then
 		FLAG=1
-		# shellcheck disable=SC2154
-		"$scriptdir"/keyboard/konsole.py
-	elif [[ $FLAG == 1 ]]
-	then
+		openrgb -p Terminal &
+	elif [[ $NAME != *"Konsole" && $FLAG == 1 ]]; then
 		FLAG=0
-		"$scriptdir"/keyboard/arch.py
+		openrgb -p Arch &
 	fi
 done < <(xprop -spy -root _NET_ACTIVE_WINDOW)
